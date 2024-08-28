4a722365635cb7bb1f09257485efc5b5f5cfa0e8
IT24103527 <it24103527@my.sliit.lk>
2024-08-28 09:35:47 +0530

    Add IT24103527Lab6Q2A to Q2A

---
+++ Q2/IT24103527Lab6Q2A.java
@@ -0,0 +1,11 @@
+ public class IT24103527Lab6Q2A {
+ public static void main(String[] args)  {
+     //Declare variables
+            int count = 1  ;
+    //Looping step
+         while(count<=10)
+     {    System.out.print(count +" ") ; 
+          count++ ;  
+                       }
+    }
+}        
